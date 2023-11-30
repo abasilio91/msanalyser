699c15f98bc1ce1b92b3820403db7373ee1e27aa
abasilio91 <adambasilio211@gmail.com>
2023-11-30 20:45:40 -0300

    created function to draw cards based on a range

--- analysistools.py
+++ analysistools.py
@@ -1,16 +1,8 @@
 #%%
 import pandas as pd
+from cardtools import *
 
 #%%
-def get_past_games(df):
-    df['past_games'] = df.apply(lambda x: list([x['num1'],
-                                                x['num2'],
-                                                x['num3'],
-                                                x['num4'],
-                                                x['num5'],
-                                                x['num6']]), axis=1)
-    return df.past_games.to_list()
-
 def create_percentage_df(df):
     percentage = pd.DataFrame(range(1,61), columns=['numero'])
     for i in range(6):
@@ -24,3 +16,5 @@ def create_percentage_df(df):
 
 def most_common_range(df, ball):
     return df[['numero', ball]].sort_values(by=ball,ascending=False)[:10]
+
+# %%
--- cardtools.py
+++ cardtools.py
@@ -1,19 +1,41 @@
 import pandas as pd
 import random
 
+def get_past_games():
+    df = pd.read_csv('raw_resultados.csv')
+    df['past_games'] = df.apply(lambda x: list([x['num1'],
+                                                x['num2'],
+                                                x['num3'],
+                                                x['num4'],
+                                                x['num5'],
+                                                x['num6']]), axis=1)
+    return df.past_games.to_list()
+
 def create_naive_cards(num_per_card: int=6, num_cards: int=1) -> list:
     games = []
-    past_games = get_past_games()
     while len(games) < num_cards:
         card = random.sample(range(1,61), num_per_card)
         card.sort()
-        if not card_exists(card, past_games):
+        if not card_exists(card):
             games.append(card)
     return games
 
-def card_exists(card, past_games):
+def card_exists(card):
+    past_games = get_past_games()
     if card in past_games:
         return True
     return False    
 
-print(create_naive_cards(6, 7))
+def create_cards_in_range(num_range: dict=None, num_cards: int=1, num_per_card: int=6):
+    if not num_range:
+        return create_naive_cards(num_per_card, num_cards)
+    
+    games = []
+    while len(games) < num_cards:
+        card = []    
+        for _, value in num_range.items():
+            card.append(random.choice(value))
+        
+        if not card_exists(card):
+            games.append(card)
+    return games
